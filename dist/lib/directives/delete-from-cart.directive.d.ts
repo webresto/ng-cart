aec8e59eab8aa626525a4a4f4af89093abb91954
larchenkov <Ivan.Larchenkov@pilot.ru>
2020-11-26 11:37:06 +0300

    delete old build

--- dist/lib/directives/delete-from-cart.directive.d.ts
+++
@@ -1,9 +0,0 @@
-import { NgRestoCartService } from '../services/ng-restocart.service';
-export declare class DeleteFromCartDirective {
-    private cartService;
-    cart: any;
-    constructor(cartService: NgRestoCartService);
-    dish: any;
-    amountDish: any;
-    onClick(): void;
-}
--- dist/public_api.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/services/ng-restocart.service';
-export * from './lib/ng-cart.module';
